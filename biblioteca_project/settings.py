12ebc0b5713dfc07246b77ef4573da81e8f737e4
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2026-02-03 11:47:47 -0700

    add migrations seems ok

--- biblioteca_project/settings.py
+++ biblioteca_project/settings.py
@@ -56,7 +56,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
-    ]
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
